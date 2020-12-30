4614fd659ecdf65c9c94c3080334d2e84d011548
KEEHY <63379191+KEEHY@users.noreply.github.com>
2020-12-30 11:09:17 +0900

    SingleKey속성.

--- Slis.Data/Dao/Dao.cs
+++ Slis.Data/Dao/Dao.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Slis.Data
 {
-    class Dao
+    public class Dao
     {
         public static BookDao Book = new BookDao();
 
---
+++ Slis.Data/Dao/RentDao.cs
@@ -0,0 +1,16 @@
+﻿using EFLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Slis.Data
+{
+    public class RentDao : SingleKeyDao<Rent, int>
+    {
+        protected override Expression<Func<Rent, int>> KeySelector => x => x.RentId;
+
+    }
+}
